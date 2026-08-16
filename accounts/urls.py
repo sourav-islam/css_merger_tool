30e53d185eb09c06b6ea780691affd7add042cdc
sourav-islam <souravislam267@gmail.com>
2026-08-16 17:53:34 +0600

    Fix logout: accept GET and redirect to login

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path("profile/edit/", views.edit_profile, name="profile_edit"),
